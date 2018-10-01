61a130a7d9207ad5c913d7b09670b0ab2744fe11
Kelsey Yim <kelseyann.yim@gmail.com>
2018-10-01 15:44:17 -0700

    Added keywords.txt

--- lexer.cpp
+++ lexer.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int check_letter(char c)
 {
-  if(isalpha(c))
+  if (isalpha(c))
   {
     return 1;
   }
-  else if(isdigit(c))
+  else if (isdigit(c))
   {
     return 2;
   }
-  else if(ispunct(c))
+  else if (ispunct(c))
   {
     return 3;
   }
@@ -27,78 +27,91 @@ int check_letter(char c)
 
 void check_state(char c, int &state, string &temp, vector<string> keywords, vector<string> tokens)
 {
-  switch(state)
+  switch (state)
   {
-    case 2:
-      for(int i = 0; i < keywords.size(); i++)
+  case 2:
+    for (int i = 0; i < keywords.size(); i++)
+    {
+      if (temp == keywords[i])
       {
-        if(temp == keywords[i])
-        {
-          cout << fixed << left << setw(15) << tokens[0] << "\t" << temp << endl;
-          state = 0;
-          temp = "";
-          break;
-        }
+        cout << fixed << left << setw(15) << tokens[0] << "\t" << temp << endl;
+        state = 0;
+        temp = "";
+        break;
       }
-      if(ispunct(c))
+    }
+    if (ispunct(c))
+    {
+      if (c == '$')
       {
-        if(c == '$')
-        {
-          temp += c;
-          cout << fixed << left << setw(15) << tokens[1] << "\t" << temp << endl;
-          state = 0;
-          temp = "";
-        }
-        else
-        {
-          cout << fixed << left << setw(15) << tokens[1] << "\t" << temp << endl;
-          temp = "";
-          state = 4;
-          check_state(c, state, temp, keywords, tokens);
-        }
+        temp += c;
+        cout << fixed << left << setw(15) << tokens[1] << "\t" << temp << endl;
+        state = 0;
+        temp = "";
       }
-      break;
-    case 3:
-      if(ispunct(c))
+      else
       {
+        cout << fixed << left << setw(15) << tokens[1] << "\t" << temp << endl;
         temp = "";
         state = 4;
         check_state(c, state, temp, keywords, tokens);
       }
-      else
-      {
-        cout << fixed << left << setw(15) << tokens[2] << "\t" << temp << endl;
-        temp = "";
-        state = 0;
-      }
-      break;
-    case 4:
-      cout << fixed << left << setw(15) << tokens[3] << "\t" << c << endl;
+    }
+    break;
+  case 3:
+    if (ispunct(c))
+    {
+      temp = "";
+      state = 4;
+      check_state(c, state, temp, keywords, tokens);
+    }
+    else
+    {
+      cout << fixed << left << setw(15) << tokens[2] << "\t" << temp << endl;
+      temp = "";
       state = 0;
-      break;
+    }
+    break;
+  case 4:
+    cout << fixed << left << setw(15) << tokens[3] << "\t" << c << endl;
+    state = 0;
+    break;
   }
 }
-
 
 void state_change(char c, int &state, string &temp, vector<string> keywords, vector<string> tokens)
 {
   int CL = check_letter(c);
-  switch(CL)
+  switch (CL)
   {
-    case 0:
-      if(state != 0) { state_change(c, state, temp, keywords, tokens); }
-      break;
-    case 1:
-      if(state == 0 || state == 2){ temp += c; state = 2; }
-      check_state(c, state, temp, keywords, tokens);
-      break;
-    case 2:
-      if(state == 0 || state == 3) { temp += c; state = 3; }
-      check_state(c, state, temp, keywords, tokens);
-      break;
-    case 3:
-      if(state == 0 || state == 4) { state = 4; }
-      check_state(c, state, temp, keywords, tokens);
+  case 0:
+    if (state != 0)
+    {
+      state_change(c, state, temp, keywords, tokens);
+    }
+    break;
+  case 1:
+    if (state == 0 || state == 2)
+    {
+      temp += c;
+      state = 2;
+    }
+    check_state(c, state, temp, keywords, tokens);
+    break;
+  case 2:
+    if (state == 0 || state == 3)
+    {
+      temp += c;
+      state = 3;
+    }
+    check_state(c, state, temp, keywords, tokens);
+    break;
+  case 3:
+    if (state == 0 || state == 4)
+    {
+      state = 4;
+    }
+    check_state(c, state, temp, keywords, tokens);
   }
 }
 
@@ -108,22 +121,36 @@ int main()
   int state = 0; // Initial state
   bool change_state = false;
   string temp = "";
-
-  vector<string> keywords = {"function", "return", "int"};
+  string word;
+  vector<string> keywords;
   vector<string> tokens = {"Keyword", "Identifier", "Integer", "Separator", "Operator"};
 
+  fstream keyw;
+  keyw.open("rat18s.txt", ios::in);
+  if (keyw.is_open()){
+    while (keyw >> word)
+    {
+      keywords.push_back(word);
+    }
+  }
+  else{
+    cerr <<"Cannot open rat18s.txt" <<endl;
+  }
+
   fstream file;
-  file.open("test.txt", ios::in);
-  if(file.is_open())
+  file.open("lexer.txt", ios::in);
+  if (file.is_open())
   {
-    while(file >> c)
+    while (file >> c)
     {
       state_change(c, state, temp, keywords, tokens);
     }
   }
   else
   {
-    cerr << "Cannot open test.txt" << endl;
+    cerr << "Cannot open lexer.txt" << endl;
   }
+  keyw.close();
+  file.close();
   return 0;
 }
